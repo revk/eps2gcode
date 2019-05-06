abd465c78c16f5f68e3164eb9f3d091f67d1ed79
RevK <github@e.gg>
2019-05-06 18:10:19 +0100

    Tweak

--- eps2gcode.c
+++ eps2gcode.c
@@ -23,7 +23,7 @@ main (int argc, const char *argv[])
    int sign = 0;
    const char *infile = NULL;
    const char *outfile = NULL;
-   double zcut = -0.5;         // Default cut depth
+   double zcut = -0.1;         // Default cut depth (assuming you have some auto levelling)
    double zskip = 5;            // Well above
    double zclear = 0.5;         // Just above
    double scale = 1;
